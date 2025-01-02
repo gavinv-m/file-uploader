10e9118332ce997009a65646e57467008adfff22
Gavin <gavinvm@protonmail.com>
2025-01-02 22:18:10 +0200

    feat: Add passport session strategy with prisma

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = express();
 // Set up session
 sessionConfig(app);
 
+// Initialise passport and passport session
+app.use(passport.initialize());
+app.use(passport.session());
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
