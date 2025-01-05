95604f59e066ee5b46a7d45e4b551c55b7e1fada
Gavin <gavinvm@protonmail.com>
2025-01-05 14:08:40 +0200

    feat: Render dashboard root

--- controller/dashboard-controller.js
+++ controller/dashboard-controller.js
@@ -2,10 +2,11 @@ import db from '../config/db/queries.js';
 
 // Exports to routes/dashboard.js
 export const renderDashboard = async (req, res) => {
-  const folderId = Number(req.params.folderId) || 0;
+  const folderId = Number(req.params.folderId) || 1;
 
   try {
     const data = await db.getFoldersAndFiles(folderId);
+    console.log(data);
     res.render('dashboard', { data });
   } catch (error) {
     console.error(`Error fetching folders and files ${file.name}:`, error);
